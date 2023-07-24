8e5ed21e964ca9b22b3ceb02c5da4280c05bf220
Emily Mabrey <emilymabrey93@gmail.com>
2023-07-24 08:14:55 -0400

    Add Eye Expressions
    Add data for I2C 8x8 matrix expressions
    Add Expressions.ino file to contain expressions declarations
    Modify I2C draw to use new expression data
    
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

--- Protogen/I2CBoard.ino
+++ Protogen/I2CBoard.ino
@@ -6,24 +6,6 @@
 
 #define I2C_REFRESH_RATE_MILLI 500
 
-static const uint8_t PROGMEM
-  smile_bmp[] = { B00111100,
-                  B01000010,
-                  B10100101,
-                  B10000001,
-                  B10100101,
-                  B10011001,
-                  B01000010,
-                  B00111100 },
-  frown_bmp[] = { B00111100,
-                  B01000010,
-                  B10100101,
-                  B10000001,
-                  B10011001,
-                  B10100101,
-                  B01000010,
-                  B00111100 };
-
 //Control object for I2C Board
 static Adafruit_8x8matrix i2cMatrix = Adafruit_8x8matrix();
 
@@ -47,9 +29,9 @@ int i2c_Thread(struct pt* pt) {
     i2cMatrix.clear();
 
     if (i2cFlipFlop) {
-      i2cMatrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
+      i2cMatrix.drawBitmap(0, 0, eye_normal, 8, 8, LED_ON);
     } else {
-      i2cMatrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
+      i2cMatrix.drawBitmap(0, 0, eye_angry2, 8, 8, LED_ON);
     }
 
     i2cMatrix.writeDisplay();
